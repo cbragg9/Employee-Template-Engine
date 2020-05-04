9ec5301b33e9c0643f2e3d2d9c759378fee82d0b
Chris Bragg <cbragg9@yahoo.com>
2020-05-03 19:40:51 -0700

    inquirer prompt looping, response handlers, rendering and writing output file

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Command line application for creating an employee summary using JavaScript, Node
 
 
 ## Latest Commit  
+-Added Inquirer Prompts  
+-Created function to handle response  
+-Created new employee instance conditional on response  
+-Rendered the output and wrote to team.html file  
+
+## Previous Commits  
 -Installed package dependencies  
 -Completed classes in "/lib" and Jest testing passed  
 -Created output folder  
--- app.js
+++ app.js
@@ -5,31 +5,102 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve("./output", "output");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let employeeArray = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const questions = [
+    {
+        type: "input",
+        message: "What is the employee's NAME?",
+        name: "EmployeeName"
+    },
+    {
+        type: "input",
+        message: "What is the employee's ID?",
+        name: "EmployeeID"
+    },
+    {
+        type: "input",
+        message: "What is the employee's E-MAIL?",
+        name: "EmployeeEmail"
+    },
+    {
+        type: "list",
+        message: "What is the employee's ROLE?",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "EmployeeRole"
+    },
+    {
+        type: "input",
+        message: "What is their office number?",
+        name: "ManagerOffice",
+        when: (answers => answers.EmployeeRole === "Manager")
+    },
+    {
+        type: "input",
+        message: "What is their github username?",
+        name: "EngineerGithub",
+        when: (answers => answers.EmployeeRole === "Engineer")
+    },
+    {
+        type: "input",
+        message: "What school are they attending?",
+        name: "InternSchool",
+        when: (answers => answers.EmployeeRole === "Intern")
+    },
+    {
+        type: "confirm",
+        message: "Would you like to add another employee?",
+        name: "RepeatInquirer",
+        default: true,
+    },
+]
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// Prompt Inquirer on app.js init
+inquirer
+    .prompt(questions)
+    .then(response => {
+        handleInquirerPrompts(response);
+    });
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function handleInquirerPrompts(response) {
+    // Variables to pass into new Employee instances
+    const newName = response.EmployeeName;
+    const newID = response.EmployeeID;
+    const newEmail = response.EmployeeEmail;
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    // Create new Employee subclasses depending on role
+    let newEmployee = "";
+    if (response.EmployeeRole === "Manager") {
+        newEmployee = new Manager(newName, newID, newEmail, response.ManagerOffice);
+    } else if (response.EmployeeRole === "Engineer") {
+        newEmployee = new Engineer(newName, newID, newEmail, response.EngineerGithub);
+    } else if (response.EmployeeRole === "Intern") {
+        newEmployee = new Intern(newName, newID, newEmail, response.InternSchool);
+    }
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    // Push new employee instance to array
+    employeeArray.push(newEmployee);
+
+    // Continue prompting inquirer if user selects Yes to confirm question
+    if (response.RepeatInquirer === true) {
+        inquirer
+            .prompt(questions)
+            .then(response => {
+                handleInquirerPrompts(response);
+            });
+    } else {
+        // Else render the employee array and write it to team.html
+        fs.writeFile(outputPath, render(employeeArray), function (err) {
+            err ? console.log(err) : console.log("team.html successfully created!");
+        });
+    }
+}
+
+// TO DO:
+// * Use validation to ensure that the information provided is in the proper expected format.
+// * Add CSS to html templates
